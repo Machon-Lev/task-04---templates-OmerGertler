67e42ce801fd9f4fb4db441690a10f09542bda7e
omre gertler <omerzki@gmail.com>
2023-06-06 17:01:51 +0300

    ex5 final

---
+++ src/PriorityQueue.cpp
@@ -0,0 +1,16 @@
+#include "PriorityQueue.h"
+
+
+
+//template<typename T>
+//inline void PriorityQueue<T>::push(const T& t)
+//{
+//	myList.push_back(t);
+//}
+//
+//template<typename T>
+//inline T PriorityQueue<T>::poll()
+//{
+//	T tmp = myList.back();
+//	return tmp;
+//}
